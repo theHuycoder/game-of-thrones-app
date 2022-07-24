e6c002daf523c4a8acd208e3a205d8b5ff731211
theHuycoder <huydo241996@gmail.com>
2022-07-24 14:55:01 +0700

    fix: add vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 import react from '@vitejs/plugin-react';
-import mix from 'vite-plugin-mix';
+import mix, { vercelAdapter } from 'vite-plugin-mix';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,7 @@ export default defineConfig({
     react(),
     mix({
       handler: './src/server/app.ts',
+      adapter: vercelAdapter(),
     }),
   ],
   resolve: {
